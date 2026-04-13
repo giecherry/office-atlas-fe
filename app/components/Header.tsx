d932e8ea85fef90b93feddbb8118f901aa871bd9
Angie C <a.cajusol98@gmail.com>
2026-04-13 20:02:53 +0200

    home link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
     return (
         <header className="fixed top-0 left-0 right-0 z-50 bg-white border-b-2 border-[#041e424a] shadow-md">
             <div className="flex items-center justify-between h-16 px-2 lg:px-16 ">
-                <div className="flex items-center gap-3">
+                <Link href="/" className="flex items-center gap-3">
                     <Image
                         src="/favicon.ico"
                         alt="OfficeAtlas"
@@ -17,7 +17,7 @@ export default function Header() {
                     <span className="text-2xl font-semibold text-[#041E42]">
                         OfficeAtlas
                     </span>
-                </div>
+                </Link>
                 <Link
                     href="/login"
                     className="text-[#041E42] hover:text-[#0a3270] transition-colors"
